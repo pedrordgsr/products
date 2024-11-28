3816146433922ac8310610987e05081aa3e56dec
Pedro Rodrigues <pedrorodriguesssrrr@gmail.com>
2024-11-28 16:09:11 -0300

    Dto and Controller

---
+++ src/main/java/com/example/springboot/controllers/ProductController.java
@@ -0,0 +1,17 @@
+package com.example.springboot.controllers;
+
+import com.example.springboot.repositories.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProductController {
+
+    @Autowired
+    ProductRepository productRepository;
+
+
+
+
+}
